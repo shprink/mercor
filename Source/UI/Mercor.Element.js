897592e5f9e1ce0051176109867b400f88f8a092
Julien Renaux <julien.renaux@gmail.com>
2012-06-11 23:11:27 +0800

    create basic structure for elements

---
+++ Source/UI/Mercor.Element.js
@@ -0,0 +1,93 @@
+/*
+---
+
+name: Mercor.Element
+
+version: 1.0
+
+description: A lightweight UI Library for Mootools
+
+authors: Julien Renaux
+
+repository: https://github.com/shprink/mercor
+
+license: MIT
+
+Copyright (c) 2012 Julien Renaux <contact@julienrenaux.fr>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+...
+ */
+
+Mercor.Element = new Class({
+
+	Implements : [ Events, Options ],
+
+	options : {
+		node : {
+			element : 'div',
+			id : '',
+			classes : '',
+			template : {},
+			styles : {}
+		}
+	},
+
+	initialize : function(options) {
+		this.setOptions(options);
+		this.setNode();
+		this.setTemplate();
+	},
+
+	setTemplate : function() {
+		// create a hash
+		// http://mootools.net/docs/more/Types/Hash
+		this.template = new Hash();
+
+		// TODO return error message
+		if(this.node) return;
+		
+		// set the close button element
+		var close = this.node.getElement('.mercor-close');
+		if (typeOf(close) == 'element') this.template.set('close', close);
+		
+		// set the header element
+		var header = this.node.getElement('.mercor-header');
+		if (typeOf(header) == 'element') this.template.set('header', header);
+		
+		// set the body element
+		var body = this.node.getElement('.mercor-body');
+		if (typeOf(body) == 'element') this.template.set('body', body);
+		
+		// set the footer element
+		var footer = this.node.getElement('.mercor-footer');
+		if (typeOf(footer) == 'element') this.template.set('footer', footer);
+	},
+
+	setNode : function() {
+		// create the basic node
+		this.node = new Element(this.options.node.element, {
+			'id' : this.options.node.id,
+			'class' : this.options.node.classes,
+			'html' : this.options.node.template,
+			'styles' : this.options.node.styles
+		});
+	}
+});
